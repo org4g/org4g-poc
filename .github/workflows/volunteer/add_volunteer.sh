db02c8c918160aafd93b9c6cee17c86d499f2c79
Nicolas Gaudin <33758361+gaudinnicolas@users.noreply.github.com>
2023-10-23 01:31:13 +0200

    Update add_volunteer.sh

--- .github/workflows/volunteer/add_volunteer.sh
+++ .github/workflows/volunteer/add_volunteer.sh
@@ -49,10 +49,10 @@ TEAM_ID=$(curl -s -H "Authorization: Bearer $ORG_TOKEN" -X POST -d "{\"query\": 
 # echo $RESPONSE
 
 # Define the role (member or maintainer)
-ROLE="maintainer"
+ROLE="member"
 
 # Make the REST API request
-RESPONSE=$(curl -X PUT -H "Authorization: bearer $ORG_TOKEN" -d "{\"role\":\"$ROLE\"}" "https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_NAME/memberships/$VOLUNTEER_USERNAME")
+RESPONSE=$(curl -H "Authorization: Bearer $ORG_TOKEN" -X PUT -d "{\"role\":\"$ROLE\"}" "https://api.github.com/orgs/$ORG_NAME/teams/$TEAM_NAME/memberships/$VOLUNTEER_USERNAME")
 echo $RESPONSE
 
 if [[ "$RESPONSE" =~ "Access not granted" ]]; then
