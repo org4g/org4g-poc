1d3ba00fff83a91a747444d28a96f1789029ea83
Nicolas Gaudin <33758361+gaudinnicolas@users.noreply.github.com>
2023-10-22 21:21:34 +0200

    Update add_volunteer.sh

--- .github/workflows/volunteer/add_volunteer.sh
+++ .github/workflows/volunteer/add_volunteer.sh
@@ -9,11 +9,9 @@ VOLUNTEER_USERNAME=$3
  
 # GraphQL Query to Get User ID
 QUERY_GET_USER_ID=$(cat <<EOF
-query {
   user(login: \"$VOLUNTEER_USERNAME\") {
     id
   }
-}
 EOF
 )
 echo $QUERY_GET_USER_ID
