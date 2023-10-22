bdc6f1977f2c1954c34ea71a7909a95423fb7869
gaudinnicolas <nicolasgaudin@yahoo.fr>
2023-10-23 00:55:30 +0200

    Needs REST Api to add member to team

--- .github/workflows/volunteer/add_volunteer.sh
+++ .github/workflows/volunteer/add_volunteer.sh
@@ -7,17 +7,17 @@ ORG_NAME=org4g
 TEAM_NAME=Volunteers
 VOLUNTEER_USERNAME=$3
  
-# GraphQL Query to Get User ID
-QUERY_GET_USER_ID=$(cat <<EOF
-{ \
- user(login: \"$VOLUNTEER_USERNAME\") { id } \
-}
-EOF
-)
-echo $QUERY_GET_USER_ID
-# Get the user ID
-USER_ID=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" -X POST -d "{\"query\": \"$QUERY_GET_USER_ID\"}" https://api.github.com/graphql | jq -r .data.user.id)
-echo $USER_ID
+# # GraphQL Query to Get User ID
+# QUERY_GET_USER_ID=$(cat <<EOF
+# { \
+#  user(login: \"$VOLUNTEER_USERNAME\") { id } \
+# }
+# EOF
+# )
+# echo $QUERY_GET_USER_ID
+# # Get the user ID
+# USER_ID=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" -X POST -d "{\"query\": \"$QUERY_GET_USER_ID\"}" https://api.github.com/graphql | jq -r .data.user.id)
+# #echo $USER_ID
 
 
 # GraphQL Query to Get Team ID
@@ -32,20 +32,27 @@ EOF
 echo $QUERY_GET_TEAM_ID
 # Get the team ID
 TEAM_ID=$(curl -s -H "Authorization: Bearer $ORG_TOKEN" -X POST -d "{\"query\": \"$QUERY_GET_TEAM_ID\"}" https://api.github.com/graphql | jq -r .data.organization.team.id)
-echo $TEAM_ID
+#echo $TEAM_ID
 
-# GraphQL Query to Add Member to Team
-QUERY_ADD_MEMBER_TO_TEAM=$(cat <<EOF
-mutation { \
-  addTeamMember(input: { teamId: \"$TEAM_ID\", username: \"$VOLUNTEER_USERNAME\" }) { \
-    clientMutationId \
-  } \
-}
-EOF
-)
-echo $QUERY_ADD_MEMBER_TO_TEAM
-# Add the user to the team
-RESPONSE=$(curl -s -H "Authorization: bearer $ORG_TOKEN" -X POST -d "{\"query\": \"$QUERY_ADD_MEMBER_TO_TEAM\"}" https://api.github.com/graphql)
+# # GraphQL Query to Add Member to Team
+# QUERY_ADD_MEMBER_TO_TEAM=$(cat <<EOF
+# mutation { \
+#   addTeamMember(input: { teamId: \"$TEAM_ID\", username: \"$VOLUNTEER_USERNAME\" }) { \
+#     clientMutationId \
+#   } \
+# }
+# EOF
+# )
+# echo $QUERY_ADD_MEMBER_TO_TEAM
+# # Add the user to the team
+# RESPONSE=$(curl -s -H "Authorization: bearer $ORG_TOKEN" -X POST -d "{\"query\": \"$QUERY_ADD_MEMBER_TO_TEAM\"}" https://api.github.com/graphql)
+# echo $RESPONSE
+
+# Define the role (member or maintainer)
+ROLE="member"
+
+# Make the REST API request
+RESPONSE=$(curl -X PUT -H "Authorization: bearer $ORG_TOKEN" -d "{\"role\":\"$ROLE\"}" "https://api.github.com/teams/$TEAM_ID/memberships/$VOLUNTEER_USERNAME")
 echo $RESPONSE
 
 if [[ "$RESPONSE" =~ "Access not granted" ]]; then
